ede64615cb29982115e1dd1fd204ceb51ab11923
sobadon <sbdn@chasoba.net>
2024-04-10 21:31:53 +0900

    add debian13

--- common/packages.mk
+++ common/packages.mk
@@ -91,6 +91,16 @@ pkg-info-debian12:
 	$(eval PKG_BASE_IMAGE = debian:bookworm)
 	$(eval PKG_SUPPORTED_PLATFORMS = linux/386 linux/amd64 linux/arm64 linux/arm/v5 linux/arm/v6 linux/arm/v7 linux/mips64le linux/ppc64le linux/s390x)
 
+.PHONY: pkg-info-debian13
+pkg-info-debian13:
+	$(eval PKG_TYPE = deb)
+	$(eval PKG_DISTRO = debian)
+	$(eval PKG_DISTRO_ID = 13)
+	$(eval PKG_DISTRO_SUITE = trixie)
+	$(eval PKG_BASE_IMAGE = debian:trixie)
+	# $(eval PKG_SUPPORTED_PLATFORMS = linux/386 linux/amd64 linux/arm64 linux/arm/v5 linux/arm/v6 linux/arm/v7 linux/mips64le linux/ppc64le linux/s390x)
+	$(eval PKG_SUPPORTED_PLATFORMS = linux/amd64)
+
 .PHONY: pkg-info-raspbian10
 pkg-info-raspbian10:
 	$(eval PKG_TYPE = deb)
